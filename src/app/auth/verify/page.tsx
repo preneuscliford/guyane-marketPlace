2e0a32051f428bfd5edf469d0ee2453aad58c58d
Cliford Preneus <preneuscliford.com>
2025-04-17 15:39:18 -0300

    fix error build

--- src/app/auth/verify/page.tsx
+++ src/app/auth/verify/page.tsx
@@ -1,7 +1,12 @@
 import dynamic from "next/dynamic";
+import { Suspense } from "react";
 
 const VerifyClient = dynamic(() => import("./verifyClient"), { ssr: false });
 
 export default function Page() {
-  return <VerifyClient />;
+  return (
+    <Suspense>
+      <VerifyClient />
+    </Suspense>
+  );
 }
