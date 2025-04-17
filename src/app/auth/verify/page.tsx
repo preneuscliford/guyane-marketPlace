d54c15de890f56d4214d16d40eff1f307eb28918
Cliford Preneus <preneuscliford.com>
2025-04-17 16:23:47 -0300

    fix error build 8

--- src/app/auth/verify/page.tsx
+++ src/app/auth/verify/page.tsx
@@ -1,15 +1,13 @@
 import dynamic from "next/dynamic";
-import { Suspense } from "react";
 
 const VerifyClient = dynamic(() => import("./verifyClient"), {
   ssr: false,
-  loading: () => <div> chargement.... </div>, // Créez un composant Spinner
 });
 
 export default function Page() {
   return (
-    <Suspense fallback={<div> chargement.... </div>}>
+    <div>
       <VerifyClient />
-    </Suspense>
+    </div>
   );
 }
